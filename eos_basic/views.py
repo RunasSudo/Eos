210ab3c999f759e09836302959b11b940990438b
RunasSudo <runassudo@yingtongli.me>
2017-01-17 14:11:45 +1030

    Basic voting booth template

--- eos_basic/views.py
+++ eos_basic/views.py
@@ -35,3 +35,7 @@ def election_view(request, election_id):
 def election_questions(request, election_id):
 	election = django.shortcuts.get_object_or_404(eos_core.models.Election, id=election_id)
 	return django.shortcuts.render(request, 'eos_basic/election_questions.html', {'election': election})
+
+def election_voting_booth(request, election_id):
+	election = django.shortcuts.get_object_or_404(eos_core.models.Election, id=election_id)
+	return django.shortcuts.render(request, 'eos_basic/election_voting_booth.html', {'election': election})
