7ea360b5d4f67e80d9ca570e36ec5f6c02d92b50
RunasSudo <runassudo@yingtongli.me>
2017-11-26 11:16:57 +1100

    Configure site title

--- eosweb/core/settings.py
+++ eosweb/core/settings.py
@@ -14,6 +14,8 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+ORG_NAME = 'FIXME'
+
 BASE_URI = 'http://localhost:5000'
 
 MONGO_URI = 'mongodb://localhost:27017/'
---
+++ local_settings.example.py
@@ -0,0 +1,12 @@
+ORG_NAME = 'Your Organisation Here'
+
+REDDIT_OAUTH_CLIENT_ID = 'xxxxxxxxxxxxxx'
+REDDIT_OAUTH_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+
+REDDIT_USER_AGENT = 'Application Title by /u/Your_Username'
+
+AUTH_METHODS = [
+	('reddit', 'Reddit')
+]
